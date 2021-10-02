0c18fe830b1cdcf1f5f2de293f20f5e9ed68c280
Matheus de Nadai <matheus.nadai@hotmail.com>
2021-10-02 20:34:00 -0300

    Validation cpf and find Statement

--- first_project/src/index.js
+++ first_project/src/index.js
@@ -1,6 +1,40 @@
+const { request, response } = require("express");
 const express = require("express");
+const { v4: uuidv4} = require("uuid")
+
+app.use(express.json());
+
+const costumers = [
+
+]
 
 
 const app = express();
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body
+    const customersAlreadyExist = customers.some((customer) => customer.cpf === cpf)
+
+    if(customersAlreadyExist){
+        return response.status(400).json({error: "Customer Already Exist!"})
+    }
+
+    customers.push({
+        cpf,
+        name, 
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send();
+
+})
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement)
+})
 
 app.listen(3333);
