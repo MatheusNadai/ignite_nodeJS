4b7f30e0bc75adc2b7cd80cb4304bdf1de302ecc
Matheus de Nadai <matheus.nadai@hotmail.com>
2021-11-02 23:24:01 -0300

    Init structure for users

---
+++ rentalx/src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,10 @@
+interface ICreateUserDTO {
+  name: string;
+  password: string;
+  email: string;
+  username: string;
+  driver_license: string;
+  id?: string;
+}
+
+export { ICreateUserDTO };
---
+++ rentalx/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,23 @@
+import { container } from "tsyringe";
+import { Response, Request, response } from "express";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  async handle(req: Request, res: Response) {
+    const { name, username, email, password, driver_license } = req.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    await createUserUseCase.execute({
+      name,
+      username,
+      email,
+      driver_license,
+      password,
+    });
+
+    return res.status(201).send();
+  }
+}
+
+export { CreateUserController };
